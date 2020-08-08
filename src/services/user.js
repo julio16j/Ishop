3f606699e8922b19bbf953401f90b15bb23b5767
Julio Cesar Urbano Vasconcelos Filho <julio.vasconcelos@academico.ifpb.edu.br>
2020-08-08 13:44:23 -0300

    Commit test

--- src/services/user.js
+++ src/services/user.js
@@ -3,19 +3,19 @@ export async function login (Email, Senha, Device = 1) {
   try {
     const response = await api.post('/store/signin',{Email, Senha, Device} )
     return response
-  } catch (err) {    
+  } catch (err) {
     return { data: { exception: 'Erro' } }
   }
 }
-export async function cadastrar (Email, Senha, Nome) { 
+export async function cadastrar (Email, Senha, Nome) {
   try {
     const response = await api.post('/store/signup',{Email, Senha, Nome} )
     return response
-  } catch (err) {    
+  } catch (err) { 
     return { data: { exception: 'Erro' } }
   }
 }
-export async function validarLogin (token) { 
+export async function validarLogin (token) {
   try {
     const response = await api.get(`/store/${token}/is-valid`)
     return response.data
@@ -23,19 +23,19 @@ export async function validarLogin (token) {
     return false
   }
 }
-export async function confirmarEmail (Email) { 
+export async function confirmarEmail (Email) {
   try {
     const response = await api.get('/store/email-check/' + Email)
     return response.data
-  } catch (err) {    
+  } catch (err) {
     return { data: { exception: 'Erro' } }
   }
 }
-export async function logout (token) { 
+export async function logout (token) {
   try {
     const response = await api.delete('/store/' + token)
     return response.data
-  } catch (err) {    
+  } catch (err) {
     return { data: { exception: 'Erro' } }
   }
 }
